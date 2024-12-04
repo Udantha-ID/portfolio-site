fd20a1e4b907236e447f5b81841fbca1966bfdfc
Udantha-ID <165542948+Udantha-ID@users.noreply.github.com>
2024-12-04 12:51:05 +0530

    Update Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <Navbar/>
         <Hero/>
+        <Grid/>
       </div>
     </main>
   );
