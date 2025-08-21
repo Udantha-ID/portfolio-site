ce64f3b9bccc3b025f3ced9faa5cce2d73737d2a
Induru Udantha <165542948+Udantha-ID@users.noreply.github.com>
2025-08-21 23:14:49 +0530

    update

--- components/experincec.tsx
+++ components/experincec.tsx
@@ -75,7 +75,7 @@ export default function experincec() {
                 initial={{opacity: 0, x: 100}}
                 transition={{duration: 1}}
                 className='w-full max-w-xl lg:w-3/4'>
-                    <p className='mb-4 text-neutral-400'>{experincec.description}</p>
+                    <p className='mb-4 text-sm sm:text-base md:text-lg text-neutral-400'>{experincec.description}</p>
                     {experincec.technologies.map((tech, index) =>(
                         <span key={index}
                               className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple'
